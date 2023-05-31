66b5a5421cbc6856d46a541e9ba7aa086b45c5fe
shivamnahar1105 <118034802+shivamnahar1105@users.noreply.github.com>
2023-05-31 12:44:08 +0530

    Update Assignment 4-2DArrays.py

--- Assignment 4-2DArrays.py
+++ Assignment 4-2DArrays.py
@@ -45,3 +45,37 @@ print(result)  # Output: [[1, 3], [4, 6]]
 
 #Q3.
 
+def transpose(matrix):
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    transpose = [[0] * num_rows for _ in range(num_cols)]
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            transpose[j][i] = matrix[i][j]
+
+    return transpose
+
+# Example usage
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = transpose(matrix)
+for row in result:
+    print(row)
+
+#Q4.
+def arrayPairSum(nums):
+    nums.sort()
+    max_sum = 0
+
+    for i in range(0, len(nums), 2):
+        max_sum += min(nums[i], nums[i+1])
+
+    return max_sum
+
+# Example usage
+nums = [1, 4, 3, 2]
+result = arrayPairSum(nums)
+print(result)  # Output: 4
+
+    
+    
